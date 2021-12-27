8c6d2d9c1570d11da77809894ef5577f901692b4
Mike <mike.ivison5@gmail.com>
2021-12-26 22:43:59 -0800

    Update newloads gui.ps1

--- newloads gui.ps1
+++ newloads gui.ps1
@@ -208,7 +208,6 @@ $Reboot.BackColor                = [System.Drawing.ColorTranslator]::FromHtml("#
 
 $Form.controls.AddRange(@($RunScript,$RunNoOEM,$UndoScript,$ExitButton,$mocologo,$nvidiashortcut,$amdshortcut,$DriverWebsites,$asusshortcut,$msishortcut,$Shortcuts,$activationbutton,$programsbutton,$updatesbutton,$ThemeButton,$powerplanbutton,$Label3,$intelshortcut,$Label4,$LightMode,$DarkMode,$Reboot))
 
-
 #region Logic 
 If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
 	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
