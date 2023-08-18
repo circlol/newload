f970a827c03bf8101872bc2ecc270c8f62ec8819
circlol <65281163+circlol@users.noreply.github.com>
2023-08-17 22:22:22 -0700

    Update newloads.ps1

--- UpdatedVersion/newloads.ps1
+++ UpdatedVersion/newloads.ps1
@@ -21,7 +21,7 @@ if ($SkipBitlocker) { $specifiedParameters += '-SkipBitlocker' }
 if ($SkipPrograms) { $specifiedParameters += '-SkipPrograms' }
 if ($WhatIf) { $specifiedParameters += '-WhatIf' }
 $parametersString = $specifiedParameters -join ', '
-Clear-Host
+#Clear-Host
 
 
 function Find-OptionalFeature {
